07544c7534532cc2f8a97718d189c8cf53aa629c
2023user <tjddn531004@naver.com>
2024-05-24 16:07:17 +0900

    파라미터 수정

--- toBusan/toBusan2.c
+++ toBusan/toBusan2.c
@@ -1,10 +1,31 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<windows.h>
-#define LEN_MIN 15// 열차 길이
+#include<time.h>
+
+// 파라미터
+#define LEN_MIN 15 // 열차 길이
 #define LEN_MAX 50
-#define PROB_MIN 10// 움직일 확률
+#define PROB_MIN 10 // 움직일 확률
 #define PROB_MAX 90
+#define STM_MIN 0 // 마동석 체력
+#define STM_MAX 5
+#define AGGRO_MIN 0 // 어그로 범위
+#define AGGRO_MAX 5
+
+// 마동석 이동 방향
+#define MOVE_LEFT 1
+#define MOVE_STAY 0
+
+// 좀비의 공격 대상
+#define ATK_NONE 0
+#define ATK_CITIZEN 1
+#define ATK_DONGSEOK 2
+
+// 마동석 행동
+#define ACTION_REST 0
+#define ACTION_PROVOKE 1
+#define ACTION_PULL 2
 
 int length, prob;
 int mc, mz, mm,turn;
